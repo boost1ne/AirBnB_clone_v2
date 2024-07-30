9ca16e038bbc106a6509b14ddadee68e73ed9e53
boost1e <boustane.chaimaa@gmail.com>
2024-07-30 09:13:00 -0700

    liner fix

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -4,12 +4,16 @@ from flask import Flask
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+
+
 @app.route('/')
 def home():
     """
     Displays Hello HBNB! msg
     """
     return 'Hello HBNB!'
+
+
 @app.route('/hbnb')
 def hbnb():
     """
@@ -17,13 +21,15 @@ def hbnb():
     """
     return 'HBNB'
 
+
 @app.route('/c/<text>')
 def c_pluse_params(text):
     """
     Displays C <text> msg
     """
-    without_underscor = text.replace('_',' ')
+    without_underscor = text.replace('_', '  ')
     return 'C {}'.format(without_underscor)
+
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
