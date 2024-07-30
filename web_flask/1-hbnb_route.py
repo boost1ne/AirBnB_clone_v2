2015720bcf39bd9e1e287e57a520a966b863b24a
boost1e <boustane.chaimaa@gmail.com>
2024-07-30 14:27:13 +0000

    update

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3
-"""Starts a Flask web application."""
+""" Starts a Flask application that MUST be lisening on 0.0.0.0, port 5000.
+    Required
+        routes: /:display "Hello HBNB!" & /hbnb: display "HBNB"
+        MUST use the option 'strict_slashes=False in route definition
+"""
 from flask import Flask
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
-@app.route('/',strict_slashes=False)
-def home():
-    """
-    Displays Hello HBNB! msg
-    """
-    return 'Hello HBNB!'
-@app.route('/hbnb',strict_slashes=False)
-def hbnb():
-    """
-    Displays HBNB msg
-    """
-    return 'HBNB'
+@app.route('/')
+def hello_hbnb():
+    """ Returns 'Hello HBNB' """
+    return "Hello HBNB!"
 
-if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+
+@app.route('/hbnb')
+def hello_hbnb2():
+    """ Returns 'HBNB' """
+    return "HBNB"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
