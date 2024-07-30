e8c70b2ec062553448e4f3f76147afacd26cfe2f
boost1e <boustane.chaimaa@gmail.com>
2024-07-30 09:00:47 -0700

    fix style

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -4,12 +4,16 @@ from flask import Flask
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+
+
 @app.route('/')
 def home():
     """
     Displays Hello HBNB! msg
     """
     return 'Hello HBNB!'
+
+
 @app.route('/hbnb')
 def hbnb():
     """
@@ -17,13 +21,16 @@ def hbnb():
     """
     return 'HBNB'
 
+
 @app.route('/c/<text>')
 def c_pluse_params(text):
     """
     Displays C <text> msg
     """
-    without_underscor = text.replace('_',' ')
+    without_underscor = text.replace('_', ' ')
     return 'C {}'.format(without_underscor)
+
+
 @app.route('/python', defaults={'text': 'is_cool'})
 @app.route('/python/<text>')
 def python_pluse_params(text='is_cool'):
@@ -32,7 +39,6 @@ def python_pluse_params(text='is_cool'):
     """
     without_underscor = text.replace('_', ' ')
     return 'Python {}'.format(without_underscor)
-
 
 
 if __name__ == "__main__":
